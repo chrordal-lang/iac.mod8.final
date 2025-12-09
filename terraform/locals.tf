04b4971f58250a61d683efb7cdc93a6901f34f2e
chrordal-lang <chrordal@gmail.com>
2025-12-09 11:23:43 +0100

    Add storage_account_name local

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,7 +1,3 @@
 locals {
-  # Eks: chrismod8-rg-dev
-  rg_name = "${var.project_name}-rg-${var.environment}"
-
-  # Eks: chrismod8sadev  (lowercase + ingen '-')
-  sa_name = lower(replace("${var.project_name}sa${var.environment}", "-", ""))
+  storage_account_name = "${var.project_name}sa${var.environment}"
 }
