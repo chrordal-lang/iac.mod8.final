30ebadedcbb9c3b3201d1035148eb695f010152a
chrordal-lang <chrordal@gmail.com>
2025-12-09 11:16:48 +0100

    Fix storage account SKU to Standard_LRS
    Updated storage account resource with new name and fixed SKU settings.

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,9 +32,13 @@ resource "azurerm_resource_group" "rg" {
 
 # Storage Account i miljøet
 resource "azurerm_storage_account" "sa" {
-  name                     = local.sa_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = var.account_tier
-  account_replication_type = var.replication_type
+  name                = local.storage_account_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  # TVING enkel og gyldig SKU for ALLE miljøer
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  min_tls_version = "TLS1_2"
 }
