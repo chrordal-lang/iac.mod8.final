0185e3730f366631ee7625a9d9d3c54f7c6c8284
Chrordal-lang <chrordal@gmail.com>
2025-12-09 07:33:33 +0100

    Use replication_type variable for storage account

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,9 +32,13 @@ resource "azurerm_resource_group" "rg" {
 
 # Storage Account i miljøet
 resource "azurerm_storage_account" "sa" {
-  name                     = local.sa_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  name                = local.storage_account_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
   account_tier             = var.account_tier
   account_replication_type = var.replication_type
+
+  # resten av felt
 }
+
